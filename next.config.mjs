29e5cfc07f7fa1d30320b16916eaa57dfc5f7f76
Shraddha <shraddhamoily392@gmail.com>
2025-11-26 11:39:55 +0530

    your update message

---
+++ next.config.mjs
@@ -0,0 +1,24 @@
+import { withSentryConfig } from "@sentry/nextjs";
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default withSentryConfig(nextConfig, {
+  
+  org: "shraddha",
+  project: "medilink",
+
+  
+  silent: !process.env.CI,
+
+  
+  
+  widenClientFileUpload: true,
+
+
+  hideSourceMaps: true,
+
+  
+  disableLogger: true,
+
+  automaticVercelMonitors: true,
+});
